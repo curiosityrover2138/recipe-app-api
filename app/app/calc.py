3da0ced28f0750582a2091b55bc90b9527fd6ace
Harsh Vardhan Tandon <Harsh.Tandon@clarivate.com>
2022-06-09 12:33:54 +0530

    Done most of the setup

---
+++ app/app/calc.py
@@ -0,0 +1,7 @@
+"""Calculator function"""
+
+def add(x,y):
+    return x+y
+
+def subtract(x,y):
+    return abs(x-y)
---
+++ app/app/tests.py
@@ -0,0 +1,15 @@
+"""Sample test"""
+from django.test import SimpleTestCase
+
+from . import calc
+
+class CalcTests(SimpleTestCase):
+    def test_add_numbers(self):
+        """test adding numbers together"""
+        res= calc.add(5,6)
+        self.assertEqual(res,11)
+    
+    def test_subtract_numbers(self):
+        """Test subtracting numbers together"""
+        res= calc.subtract(4,2)
+        self.assertEqual(res,2)
